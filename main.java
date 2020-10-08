21c8e7f81db25e07841b50af692a482c85924778
jy2575 <jasonyu@yuda.localdomain>
2020-10-08 12:33:53 -0700

    Recipe Retrieval feature update

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 import java.util.*;
+import java.io.*;
 
-class Recipe {
+class Recipe implements Serializable {
+	private static final long serialVersionUID = 1L;
 	String name;
 	String description;
 	ArrayList<String> ingredients;
@@ -23,12 +25,12 @@ class Recipe {
 
 	public void addRecipe() {
 		Scanner input_recipe = new Scanner(System.in);
-		System.out.println("Enter the name of your recipe:");
+		System.out.println("\nEnter the name of your recipe:");
 		this.name = input_recipe.nextLine();
-		System.out.println("Enter the description of your recipe:");
+		System.out.println("\nEnter the description of your recipe:");
 		this.description = input_recipe.nextLine();
-		System.out.println("Enter the number of ingredients you would like to add");
-		// number of ingredients bug updated
+		System.out.println("\nEnter the number of ingredients you would like to add");
+
 		int num_ingredients = Integer.parseInt(input_recipe.nextLine());
 		int counter = 0;
 		while (counter < num_ingredients) {
@@ -37,8 +39,8 @@ class Recipe {
 			this.ingredients.add(ing_str);
 			counter++;
 		}
-		// same structure applied to number of instructions
-		System.out.println("Enter the number of instructions you would like to add");
+
+		System.out.println("\nEnter the number of instructions you would like to add");
 		int num_instructions = Integer.parseInt(input_recipe.nextLine());
 		counter = 0;
 		while (counter < num_instructions) {
@@ -49,39 +51,145 @@ class Recipe {
 		}
 	}
 
-	/*
-	 * name, description, ingredients, instructions donut, "a donut making recipe",
-	 * "dough, sugar, milk, egss"
-	 */
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+
+		for (String s : this.ingredients) {
+			sb.append(s + " ");
+		}
+		String str_ingr = sb.toString();
+		sb = new StringBuffer();
+		for (String x : this.instructions) {
+			sb.append((this.instructions.indexOf(x) + 1) + "." + x);
+			sb.append("\n");
+		}
+		String str_instr = sb.toString();
+
+		return "Recipe Name: " + this.name + "\n" + "Recipe Description: " + this.description + "\n"
+				+ "Recipe Ingredients: " + str_ingr + "\n" + "Recipe Instructions:\n" + str_instr;
+
+	}
 
 }
 
 public class main {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println(
+				"Welcome to the Team Gusteau Recipe Application! To begin, enter a command or type HELP to see a list of commands:");
+		Scanner userInput = new Scanner(System.in);
+		String user_command;
 
-		Recipe recipeObject = new Recipe();
-		recipeObject.addRecipe();
+		Recipe my_rec;
+		ArrayList<Recipe> menu = new ArrayList<Recipe>();
+		while (true) {
+			user_command = userInput.nextLine();
+			if (user_command.toLowerCase().equals("create")) {
+				System.out.println(
+						"\nLet's make a new recipe! You'll be prompted to enter several components of your recipe, please hit enter after each entry:");
+				my_rec = new Recipe();
+				my_rec.addRecipe();
+				menu.add(my_rec);
+				System.out.println("\n" + my_rec.toString());
+				System.out.println("What would you like to do next?");
+				try {
+					FileOutputStream f = new FileOutputStream("myRecipes.txt");
+					ObjectOutputStream o = new ObjectOutputStream(f);
+					// Write objects to file
+					o.writeObject(my_rec);
+					o.close();
+					f.close();
 
-		StringBuffer sb = new StringBuffer();
+				} catch (FileNotFoundException e) {
+					System.out.println("File not found");
+				} catch (IOException e) {
+					System.out.println("Error initializing stream");
+				}
+			}
 
-		StringBuffer sb2 = new StringBuffer();
+			else if (user_command.toLowerCase().equals("find")) {
+				System.out.println(
+						"\nLooking for a recipe? Enter 1 to search for a recipe by name, or enter 2 to browse all exsiting recipes:");
+				Scanner userInput1 = new Scanner(System.in);
+				String user_command1 = userInput1.nextLine();
 
-		for (String s : recipeObject.ingredients) {
-			sb.append(s);
-			sb.append(" ");
+				boolean Found = false;
+				if (user_command1.toLowerCase().equals("1")) {
+					System.out.println("\nPlease enter the recipe name:");
+					Scanner userInput2 = new Scanner(System.in);
+					String user_command2 = userInput2.nextLine();
+					for (Recipe r : menu) {
+						if (r.name.toLowerCase().equals(user_command2.toLowerCase())) {
+							System.out.println("\nRecipe found!\n");
+							System.out.println(r.toString());
+
+							/* The Recipe Exploration feature goes here */
+							Found = true;
+
+							break;
+						}
+
+					}
+					if (!Found) {
+						System.out.println("\nSorry recipe wasn't found...\n");
+					}
+
+				} else if (user_command1.toLowerCase().equals("2")) {
+					System.out.println();
+					printMenu(menu);
+
+					/* The Recipe Exploration feature goes here */
+
+				}
+
+				System.out.println("What would you like to do next?");
+
+			}
+
+			else if (user_command.toLowerCase().equals("read")) {
+				try {
+					FileInputStream fi = new FileInputStream("myRecipes.txt");
+					ObjectInputStream oi = new ObjectInputStream(fi);
+					boolean keepReading = true;
+					try {
+						while (keepReading) {
+							Recipe read_rec = (Recipe) oi.readObject();
+							System.out.println(read_rec.toString());
+							menu.add(read_rec);
+							System.out.println("What would you like to do next?");
+
+						}
+					} catch (EOFException e) {
+						keepReading = false;
+					}
+
+					oi.close();
+					fi.close();
+
+				} catch (FileNotFoundException e) {
+					System.out.println("File not found");
+				} catch (IOException e) {
+					System.out.println("Error initializing stream");
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (user_command.toLowerCase().equals("exit")) {
+				System.out.println("\nGoodbye Chef!");
+				break;
+			}
 		}
-		// let instructions be printed as well
-		for (String s : recipeObject.instructions) {
-			sb2.append(s);
-			sb2.append(" ");
+
+	}
+
+	public static void printMenu(ArrayList<Recipe> r) {
+		StringBuilder sb = new StringBuilder();
+		for (Recipe s : r) {
+			sb.append(s.name);
+			sb.append("\n");
 		}
 
-		String str = sb.toString();
-		String str2 = sb2.toString();
-		System.out
-				.println("Recipe Name: " + recipeObject.name + "\n" + "Recipe Description: " + recipeObject.description
-						+ "\n" + "Recipe Ingredients: " + str + "\n" + "Recipe Instructions: " + str2 + "\n");
+		System.out.println(sb.toString());
 
 	}
 }
