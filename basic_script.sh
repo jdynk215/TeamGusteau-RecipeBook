caa09a5fb81a08dc0556b8764390016b3c8afae4
Joe Dynk <joedynk@Joes-MBP-3.lan>
2020-10-16 02:49:52 -0400

    modified the script to check wether the user already has the repo cloned

--- basic_script.sh
+++ basic_script.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-git clone https://github.com/jdynk215/TeamGusteau-RecipeBook
-cd TeamGusteau-RecipeBook
-javac main.java
-java main
-cd ..
-rm -rf TeamGusteau-RecipeBook
+FILE=TeamGusteau-RecipeBook
+if [ -d "$FILE" ]; then
+    echo "$FILE exists."
+    cd TeamGusteau-RecipeBook
+	javac main.java
+	java main
+    
+else 
+    echo "$FILE does not exist."
+    git clone https://github.com/jdynk215/TeamGusteau-RecipeBook
+	cd TeamGusteau-RecipeBook
+	rm -rf basic_script.sh
+	javac main.java
+	java main
+fi
+
